33c1d7c7e22d4b3605fa77c3833c2c230234844f
HelloCodeMing <huanmingwong@163.com>
2015-07-01 14:53:05 +0800

    finish ExecTime

--- include/ming/ExecTime.hpp
+++ include/ming/ExecTime.hpp
@@ -1,8 +1,15 @@
+#ifndef MING_EXECTIME_HPP
+#define MING_EXECTIME_HPP
+
+#include <ctime>
 namespace ming { 
 namespace time { 
-namespace detail {
 
-
+/**
+ * @desc: Compute the execution(CPU) time of a function.
+ * @para: A function.
+ * @ret : Execution time in millisecond.
+ */
 template <class FN>
 long ExecTime(FN f) {
     clock_t start = clock();
@@ -11,7 +18,9 @@ long ExecTime(FN f) {
     return 1000 * (end - start) / CLOCKS_PER_SEC;
 }
 
-} /* end of namespace ming::time::detail */
 } /* end of namespace ming::time */
+
+using time::ExecTime;
 } /* end of namespace ming */
 
+#endif 
